3fd00515abffd1aa2c882774e43afc8afce49562
JirakJ <jirak92@gmail.com>
2020-03-11 07:42:22 +0100

    Add method for decode text from given integers

--- src/com/jakubjirak/Main.java
+++ src/com/jakubjirak/Main.java
@@ -7,9 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	    // write your code here
         String code = "48 61 76 65 20 79 6f 75 20 73 65 65 6e 20 74 68 65 20 73 6f 75 72 63 65 20 63 6f 64 65 20 6f 66 20 74 68 65 20 4a 65 74 42 72 61 69 6e 73 20 77 65 62 73 69 74 65 3f";
-        Arrays.stream(code.split(" "))
-                .map((s) -> (char)Integer.decode("0x" + s).intValue())
-                .forEach(System.out::print);
+        decodeIntegerToAscii(code);
 
         printClueFromWebsite();
         System.out.println("Goto https://www.jetbrains.com/products.html# and find out product JK, than click on learn more.");
@@ -96,12 +94,18 @@ public class Main {
                     break;
                 }
             }
-            
+
             if(isPrime) {
                 count++;
             }
         }
         return count;
+    }
+
+    public static void decodeIntegerToAscii(String code){
+        Arrays.stream(code.split(" "))
+                .map((s) -> (char)Integer.decode("0x" + s).intValue())
+                .forEach(System.out::print);
     }
 
     public static String encrypt (String srcString, int shift)
